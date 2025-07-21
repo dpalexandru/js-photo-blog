a5ec7d18c19aedfbd438647cf0ecb2c133e4ea52
dpalexandru <alexpoian91@hotmail.com>
2025-07-21 16:40:15 +0200

    fix: full data card innerHTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,14 @@ const imgLink = 'https://lanciweb.github.io/demo/api/pictures/';
 
 
 //Chiamata axios per ottenere dati img 
-
 axios.get(imgLink).then(resp => {
     //recupero i dati 
     const immages = resp.data;
 
-    //Ciclo per inserire i dati recuperati in card
+    //Ciclo immages per inserire i dati recuperati in card
     for (i = 0; i < immages.length; i++) {
-        //Destructuring a ogni ciclo
 
+        //Destructuring a ogni ciclo
         const { title, url, date } = immages[i];
 
         cardContainer.innerHTML += `
@@ -22,10 +21,10 @@ axios.get(imgLink).then(resp => {
 
                     <!--Card stile polaroid-->
                     <div class="card-polaroid">
-                        <img id="card-img" src="https://i1.sndcdn.com/artworks-v9HOpFB6xe6LV3H3-nkTBBg-t500x500.png" alt="">
+                        <img id="card-img" src="${url}" alt="">
                         <div class="c-text mt-2">
-                            <span id="data">10-01-1991</span>
-                            <h3 id="titolo-card">Titolo</h3>
+                            <span id="data">${date}</span>
+                            <h3 id="titolo-card">${title}</h3>
                         </div>
                         <div class="pin-container">
                             <img id="pin" src="./assets/img/pin.svg" alt="">
@@ -34,5 +33,4 @@ axios.get(imgLink).then(resp => {
                 </div>
         `
     }
-
 });
